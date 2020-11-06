72657530079a915d0eabbecb82adfa18b23e520d
OoiuyyuioO <ooiuyyuioo@gmail.com>
2020-11-06 16:36:57 +0500

    started project and added assets folders,settings main scene

---
+++ Assets/Scenes/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text MoneyText;
+    public Text DPCtext;
+    public Text StageText;
+    public Text KillsText;
+
+    public Button Back;
+    public Button Forward;
+
+
+}
